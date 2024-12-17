a0f55729ca219c73cb0b01ea7bcecfcee744b9dd
ewg-smir <ewg.smir.2002@gmail.com>
2024-12-17 15:57:51 +0300

    1

--- src/js/index.js
+++ src/js/index.js
@@ -5,13 +5,16 @@ import '../js/tehnic.js';
 import '../js/price.js';
 import '../js/menu.js';
 import '../js/modalFeedback.js';
+import '../js/modalCall.js';
 import { closeMenu, menus } from '../js/menu.js';
 import { feedback } from '../js/modalFeedback.js';
 import { services } from '../js/services.js';
+import { feedCall } from '../js/modalCall.js';
 
 window.addEventListener("resize", () => {
   services();
   menus();
   feedback();
   closeMenu();
+  feedCall();
 });
